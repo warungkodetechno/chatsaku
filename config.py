00b8931a612112c76c1cc8cf22332dd4e07c8eae
bambangimamh <bambangimamh@gmail.com>
2026-07-26 17:41:37 +0700

    system login 4

--- config.py
+++ config.py
@@ -4,25 +4,17 @@ from flask_cors import CORS
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
-cors = CORS()
-
 
 def init_extensions(app):
 
     bcrypt.init_app(app)
-
     jwt.init_app(app)
 
-    cors.init_app(
+    CORS(
         app,
-        resources={
-            r"/api/*": {
-                "origins": [
-                    "http://localhost:5500",
-                    "http://127.0.0.1:5500",
-                    "https://chatsaku.com",
-                    "https://www.chatsaku.com"
-                ]
-            }
-        }
+        origins=[
+            "https://chatsaku.com",
+            "https://www.chatsaku.com"
+        ],
+        supports_credentials=True
     )
